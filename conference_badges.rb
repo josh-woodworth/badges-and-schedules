e4d744a583f7989e9d04fe9037a0ae233dc81839
Josh Woodworth <72992058+josh-woodworth@users.noreply.github.com>
2021-01-19 12:16:33 -0700

    commit tree 68aead3abd1d9f216d18fda5e438279beccb5320

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(names)
+    return "Hello, my name is #{names}."
+end
+
+def batch_badge_creator(names)
+    badges = []
+    names.each {|name| badges << (badge_maker(name))}
+    return badges
+  end
+
+def assign_rooms(attendees)
+    room_assignments = []
+    attendees.each_with_index{|name, room| room_assignments << "Hello, #{name}! You'll be assigned to room #{room + 1}!"} 
+    return room_assignments
+end
+
+def printer(arr)
+    batch_badge_creator(arr).each do |badge|
+        puts badge
+    end
+
+    assign_rooms(arr).each do |room|
+        puts room
+    end
+end
